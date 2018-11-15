c9e7aa3c8b8e0e646dbb57bc0a94e98de34fa601
maciejcieslar <marekmckl@gmail.com>
2018-11-15 21:16:13 +0100

    adding redux poc

---
+++ src/redux.test.ts
@@ -0,0 +1,49 @@
+import { combineReducers, createStore } from './redux'
+
+describe('Redux', () => {
+  it('creates a new state after each dispatched action', () => {
+    const rootReducer = combineReducers({
+      todos: (state = [], action) => {
+        switch (action.type) {
+          case 'ADD_TODO': {
+            return [...state, { activity: action.payload.activity }]
+          }
+        }
+
+        return [...state]
+      },
+      user: (state = { firstName: '', lastName: '' }, action) => {
+        switch (action.type) {
+          case 'UPDATE_USER': {
+            return {
+              ...state,
+              ...action.payload.data,
+            }
+          }
+        }
+
+        return { ...state }
+      },
+    })
+
+    const store = createStore(rootReducer)
+
+    store.subscribe((state, prevState) => {
+      expect(state.user).not.toBe(prevState.user)
+      expect(state.user).toEqual({ firstName: 'Maciej', lastName: 'Cieslar' })
+      expect(prevState.user).toEqual({ firstName: '', lastName: '' })
+    })
+
+    store.dispatch({
+      type: 'UPDATE_USER',
+      payload: {
+        data: {
+          firstName: 'Maciej',
+          lastName: 'Cieslar',
+        },
+      },
+    })
+
+    expect(store.getState()).not.toBe(store.getState())
+  })
+})
---
+++ src/redux/index.ts
@@ -0,0 +1,44 @@
+function copy<T>(obj: T): T {
+  return JSON.parse(JSON.stringify(obj))
+}
+
+const combineReducers = (reducersMap: object) => {
+  return (state = {}, action) => {
+    return Object.keys(reducersMap).reduce((result, key) => {
+      const reducer = reducersMap[key]
+      const stateSlice = state[key]
+
+      return {
+        ...result,
+        [key]: reducer(stateSlice, action),
+      }
+    }, {})
+  }
+}
+
+const createStore = (reducer, initialState: any = {}) => {
+  let state = {
+    ...copy(initialState),
+    ...reducer(undefined, {}),
+  }
+  let subscribers = []
+
+  return {
+    getState() {
+      return copy(state)
+    },
+    subscribe(subscriber) {
+      subscribers = [...subscribers, subscriber]
+    },
+    dispatch(action) {
+      const prevState = state
+      state = reducer(state, action)
+
+      subscribers.forEach((sub) => sub(state, prevState))
+
+      return action
+    },
+  }
+}
+
+export { createStore, combineReducers }
